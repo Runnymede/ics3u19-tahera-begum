f77c8db5522ed7dd21f0430997766db81e43bd16
333774396 <333774396@C-5Y38GQ1.tdsb.on.ca>
2019-11-25 13:45:37 -0500

    unit3 clappyhands and namesarray

---
+++ ICS3U-Tahera Begum/src/begum/unit3/ClappyAndYouKnowIt.java
@@ -0,0 +1,28 @@
+package begum.unit3;
+
+import java.util.Scanner;
+
+public class ClappyAndYouKnowIt {
+
+	public static void main(String[] args) {
+		
+		
+
+		for (int i = 0; i < num; i++)
+		{
+
+			names [i] = sc.next();
+
+		}
+
+		for (int i = 0; i < num; i++)
+		{
+
+			System.out.println(names [i]);
+
+		}
+
+
+	}
+
+}
--- ICS3U-Tahera Begum/src/begum/unit3/NamesArray.java
+++ ICS3U-Tahera Begum/src/begum/unit3/NamesArray.java
@@ -1,11 +1,44 @@
 package begum.unit3;
 
-
+import java.util.Scanner;
 public class NamesArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//Scanner
+		Scanner sc = new Scanner(System.in);
+
+		//asks the user for a list of names
+		System.out.println("Enter the number of names you would like in your list.");
 		
+		int num = sc.nextInt();
+		
+		String [] names = new String [num];
+		
+		System.out.println("Enter the " + " names one by one");
+		
+		for (int i = 0; i < num; i++)
+		{
+			
+			names [i] = sc.next();
+			 	
+		}
+		
+		for (int i = 0; i < num; i++)
+		{
+			
+			System.out.println(names [i]);
+			
+		}
+		
+		
+		for (int i = num - 1; i >= 0; i--)
+		{
+			
+			System.out.println(names [i]);
+			
+		}
+		sc.close();
 		
 
 	}
