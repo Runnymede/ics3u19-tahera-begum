9c63246fc2fa9f1b47b7db4ebcad326a6e9d6ffa
333774396 <333774396@C-9SM7WR1.tdsb.on.ca>
2020-01-06 15:08:20 -0500

    math plus

--- ICS3U-Tahera Begum/src/begum/unit4/MathPlus.java
+++ ICS3U-Tahera Begum/src/begum/unit4/MathPlus.java
@@ -5,35 +5,35 @@ import java.util.Scanner;
 public class MathPlus {
 
 	public static void main(String[] args) {
-		
+
 		//Scanner
 		Scanner sc = new Scanner(System.in);
 
-		//asks the user for a list of names
+		//asks the user for a list of integers
 		System.out.println("Enter the number of integers you would like in your list.");
 
 		//scans the number entered by the user and declares it as num
 		int num = sc.nextInt();
 
-		//an array for the list of names that will be entered by the user
+		//an array for the list of integers that will be entered by the user
 		String [] integers = new String [num];
 
-		//tells the user to enter the names one by one
+		//tells the user to enter the integers one by one
 		System.out.println("Enter the integers one by one");
 
-		//loops the number of times the number of names
+		//loops the number of times the number of integers
 		for (int i = 0; i < num; i++)
 		{
 			//scans the name puts it in the array
 			integers [i] = sc.next();
 
 		}
-		
-		public static int min() {
-			
-			System.out.println("");
-		}
+		min(String [] integers);
 
 	}
 
+	public static int min(String [] integers) {
+
+		System.out.println("");
+	}
 }
